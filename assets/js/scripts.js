2bd1bfdcafd5d915c7c92bf9e372e714a914585c
Dustin Smiddie <101125377+DSmiddie3@users.noreply.github.com>
2022-07-04 06:05:37 -0400

    4.2.6

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,10 +4,21 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // creat list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    //creat div to hold task info and add to list item
+    var taskInfoE1 = document.createElement("div");
+    taskInfoE1.className = "task-info";
+    // Add HTML content to div
+    taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskNameInpt + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemE1.appendChild(taskInfoE1);
+    // Add entire list item to the list
     tasksToDoEl.appendChild(listItemEl);
+    
 };
 
 //buttonEl.addEventListener("click", createTaskHandler); 
